efbdedec336cc19afaa32c7d18a28ccf8f514632
Phil <8772256+phillmp@users.noreply.github.com>
2024-04-30 15:16:06 -0700

    fixing links for github pages, it hates leading slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://littlewindlabs.github.io',
   integrations: [mdx(), sitemap()]
   // integrations: [mdx(), sitemap(), svelte(), tailwind()]
 });
